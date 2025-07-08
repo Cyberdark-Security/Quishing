38b4309dc7169a8fe3e0f8766da1a0e59682e821
Cyberdark <196129026+Cyberdark-SecOS@users.noreply.github.com>
2025-07-07 21:12:11 -0500

    inicio app

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Iniciando la aplicación con Gunicorn..."
-gunicorn --bind 0.0.0.0:8000 --timeout 600 --workers 4 back:app
+gunicorn --bind 0.0.0.0:8000 --timeout 600 --workers 4 app:app
